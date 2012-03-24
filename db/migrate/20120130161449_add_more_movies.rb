e58b9a235a455166db0c2a4d7c5429d033dd9329
Ubuntu <ubuntu@domU-12-31-39-09-FA-C9.compute-1.internal>
2012-03-24 09:39:50 +0000

    sd

--- db/migrate/20120130161449_add_more_movies.rb
+++ db/migrate/20120130161449_add_more_movies.rb
@@ -1,15 +1,15 @@
 class AddMoreMovies < ActiveRecord::Migration
   MORE_MOVIES = [
-    {:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992'},
-    {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984'},
-    {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989'},
-    {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011'},
-    {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001'},
-    {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001'},
-    {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968'},
-    {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004'},
-    {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'},
-    {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000'},
+    {:title => 'Aladdin', :rating => 'G', :release_date => '25-Nov-1992', :director=>'222'},
+    {:title => 'The Terminator', :rating => 'R', :release_date => '26-Oct-1984', :director=>'555'},
+    {:title => 'When Harry Met Sally', :rating => 'R', :release_date => '21-Jul-1989', :director=>'777'},
+    {:title => 'The Help', :rating => 'PG-13', :release_date => '10-Aug-2011', :director=>'888'},
+    {:title => 'Chocolat', :rating => 'PG-13', :release_date => '5-Jan-2001', :director=>'555'},
+    {:title => 'Amelie', :rating => 'R', :release_date => '25-Apr-2001', :director=>'aaa'},
+    {:title => '2001: A Space Odyssey', :rating => 'G', :release_date => '6-Apr-1968', :director=>'222'},
+    {:title => 'The Incredibles', :rating => 'PG', :release_date => '5-Nov-2004', :director=>'666'},
+    {:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981', :director=>'666'},
+    {:title => 'Chicken Run', :rating => 'G', :release_date => '21-Jun-2000', :director=>'666'},
   ]
   def up
     MORE_MOVIES.each do |movie|
